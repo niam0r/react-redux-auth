fbd10b821fccb188e8ded2e966c250589298c46b
Romain niam0r <niamor@mm.st>
2018-04-04 19:00:49 +0100

    implementing index.js and npm i --save nodemon

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 // main start point of the app
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on:', port);
