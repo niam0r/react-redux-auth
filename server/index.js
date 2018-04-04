8781c3a3b78464f66bca692077800e7cc663f3d2
Romain niam0r <niamor@mm.st>
2018-04-04 19:06:04 +0100

    dev: nodemon index.js

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
 
+
 // Server setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
 console.log('server listening on:', port);
+
+// nodemon is a sort of hot restarter for the server folder
